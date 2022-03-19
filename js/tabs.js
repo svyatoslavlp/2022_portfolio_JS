70a31dc43f27b147595522b8b95db56330374519
Svyatoslav <svyatoslavlp@mail.ru>
2022-03-19 21:59:04 +0500

    Переключение по индексу

--- js/tabs.js
+++ js/tabs.js
@@ -1,3 +1,34 @@
-const main = document.getElementById('main')
+const tabButtons = document.querySelectorAll('.design-list__item')
+const tabDescriptions = document.querySelectorAll('.design__descr')
+const tabImages = document.querySelectorAll('.design-images')
 
-console.log(main);
+console.log(tabImages);
+
+tabButtons.forEach((tabButton, index) => {
+   tabButton.addEventListener('click', (event) => {
+      tabDescriptions.forEach((descr, indexDescr) => {
+         if (index === indexDescr) {
+            descr.classList.remove('hidden')
+         } else {
+            descr.classList.add('hidden')
+         }
+      })
+
+      tabImages.forEach((image, indexImage) => {
+         if (index === indexImage) {
+            image.classList.remove('hidden')
+         } else {
+            image.classList.add('hidden')
+         }
+      })
+
+      tabButtons.forEach((btn) => {
+         if (btn === event.target) {
+            btn.classList.add('design-list__item_active')
+         } else {
+            btn.classList.remove('design-list__item_active')
+         }
+
+      })
+   })
+})
