850f4fd21cb5d3da53272bdd8553f5ec02e1e3c4
Svyatoslav <svyatoslavlp@mail.ru>
2022-03-20 23:32:41 +0500

    Работает таймер

--- js/timer.js
+++ js/timer.js
@@ -1,9 +1,13 @@
 timerBlock = document.querySelector('.timer__time')
 const updateClock = () => {
-   const date = new Date()
-   const hourse = date.getHours()
-   const minutes = date.getMinutes()
-   const seconds = date.getSeconds()
+   const date = new Date().getTime()
+   const dateDeadline = new Date('31 march 2022').getTime()
+   const timeRemaining = (dateDeadline - date) / 1000
+
+   const hourse = Math.floor(timeRemaining / 60 / 60)
+   const minutes = Math.floor((timeRemaining / 60 / 60) % 60)
+   const seconds = Math.floor(timeRemaining % 60)
+
    const fHours = hourse < 10 ? '0' + hourse : hourse
    const fMinutes = minutes < 10 ? '0' + minutes : minutes
    const fSeconds = seconds < 10 ? '0' + seconds : seconds
